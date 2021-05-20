2c1e15a4f1a0484e3b9f41e4f8f5939ff87a199b
akt47 <78057009+akt47@users.noreply.github.com>
2021-05-20 18:24:46 -0500

    developing index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: "input",
+        name: "github",
+        message : "What's your Github username"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    returnfs.writeFile(path.join(process.cwd(), fileName),data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
