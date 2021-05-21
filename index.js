62c9c32fd58f0494e89572193cc903ca8e909389
akt47 <78057009+akt47@users.noreply.github.com>
2021-05-21 14:12:00 -0500

    initialize questions

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path")
+const generateRM = require("./generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = [
 
     {
         type: "input",
-        name: "github",
-        message : "What's your Github username"
-    }
+        name: "email",
+        message : "email?"
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -16,7 +20,13 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers) => {
+        console.log("Wait one moment")
+        writeToFile("Rea")
+    })
+}
 
 // Function call to initialize app
 init();
